3ed862aacb43f358aa75b4781f164e285f2e07df
Shubham-tiwari123 <st540362@gmail.com>
2020-07-22 10:53:12 +0530

    adds script for memory monitoring and some fixes

--- monitor/test_node.py
+++ monitor/test_node.py
@@ -1,58 +1,112 @@
-import subprocess
+import sys
 import os
-import requests
+import subprocess
 import time
+import requests
+import shutil
 
-# stores besu-sample-networks folder location.
-directoryLocation = "/root/besu/besu-sample-networks/"
+resume_count = 0
+waiting_time = 0
+sleep_time = 0
+
+# Error codes
+connection_error = 111
+http_error = 404
+fail= 500
+success = 200
+other_exception = 300
 
 
-# stores grafana api url
-url = "http://localhost:3000/api/datasources/proxy/1/api/v1/query?query=sum%20by%20(instance)%20(ethereum_blockchain_height%7Binstance%3D~%22bootnode%3A9545%7Cminernode%3A9545%7Cnode1%3A9545%7Cnode2%3A9545%7Cnode3%3A9545%7Crpcnode%3A9545%22%7D)"
+def check_storage():
+	print("\nGetting memory usage...")
+	total, used, free = shutil.disk_usage("/")
 
-# Count to store how many times container is tried to resume. If count exceeds 2 times then the container is removed 
-# and it is reinstalled again.
-resumeCount =0
+	total_memory = (total // (2**30))
+	used_memory = (used // (2**30))
+	free_memory = (free // (2**30))
 
-# waiting time for grafana to start.
-waitingTime = 180
+	print("Total: %d GiB" % total_memory)
+	print("Used: %d GiB" % used_memory)
+	print("Free: %d GiB" % free_memory)
+
+	if free_memory > 5:
+		return success
+	print("Memory is filled...")
+	return fail
 
 
-# interval time between two process
-sleepTime =10
-
-# command to navigate to desired location
-os.chdir(directoryLocation)
-
-while (True):
-	print("Getting api status..... \n")
+def get_node_status(api_url, total_node):
 	try:
-		response = requests.get(url)
-		responseData = response.json()
-		print(responseData)
-		resultArray = responseData["data"]["result"]
-		print(len(resultArray))
-		if len(resultArray)!=6:
+		print("\nGetting grafana api response")
+		response = requests.get(api_url)
+		response_data = response.json()
+		# print(response_data)
+		result_array = response_data["data"]["result"]
+		print("\nResponse length: %d" % len(result_array))
+		if (len(result_array)) != total_node:
 			print("\nStopping.....")
-			# remove first parameter sudo if docker does not need sudo to perform
-			subprocess.call(["sudo","./stop.sh"])
-			time.sleep(sleepTime)
-			if resumeCount<2:
+			subprocess.call(["./stop.sh"])
+			time.sleep(sleep_time)
+			global resume_count
+			if resume_count < 2:
 				print("\nResuming.......")
-				# remove first parameter sudo if docker does not need sudo to perform
-				subprocess.call(["sudo","./resume.sh"])
-				resumeCount = resumeCount+1
-				time.sleep(waitingTime)
-	except:
-		resumeCount=0
-		print("\nRemoving.......\n")
-		# remove first parameter sudo if docker does not need sudo to perform
-		subprocess.call(["sudo","./remove.sh"])
-		time.sleep(sleepTime)
-		print("\nReinstalling......\n")
-		# remove first parameter sudo if docker does not need sudo to perform
-		subprocess.call(["sudo","./run-privacy.sh"])
-		time.sleep(waitingTime)
+				subprocess.call(["./resume.sh"])
+				resume_count = resume_count+1
+				time.sleep(waiting_time)
+				return success
+			else:
+				resume_count = 0
+				return fail
+		return success
+	except requests.exceptions.ConnectionError as conn_err:
+		print("Connection error", conn_err)
+		resume_count = 0
+		return connection_error
+	except requests.exceptions.HTTPError as http_err:
+		print("Http Error:", http_err)
+		return http_error
+	except Exception as exception:
+		print("Exception:", exception)
+		return other_exception
 
-	time.sleep(sleepTime)
-	
+
+def run_script(api_url, total_node):
+
+	while True:
+		storage_status = check_storage()
+		node_status = get_node_status(api_url, total_node)
+		if storage_status == fail or node_status == fail or node_status == connection_error:
+			print("\nRemoving.......\n")
+			# subprocess.call(["./remove.sh"])
+			# time.sleep(sleep_time)
+			# print("\nReinstalling......\n")
+			# subprocess.call(["./run-privacy.sh"])
+			# time.sleep(waiting_time)
+		elif node_status == http_error or node_status == other_exception:
+			print("Terminating the program")
+			break
+		else:
+			print("All good")
+		time.sleep(sleep_time)
+
+
+if __name__ == "__main__":
+
+	# Passing arg from command line:
+	# Command to run: python3 test.py ../besu/besu-sample-networks/ 0 180 10 6
+	# First arg: location
+	# Second arg: resume_count
+	# Third arg: waiting_time
+	# Fourth arg: sleep_time
+	# Fifth arg: total_nodes
+
+	directory_location = sys.argv[1]
+	resume_count = int(sys.argv[2])
+	waiting_time = int(sys.argv[3])
+	sleep_time = int(sys.argv[4])
+	total_nodes = int(sys.argv[5])
+	url = "http://localhost:3000/api/datasources/proxy/1/api/v1/query?query=sum%20by%20(instance)%20(ethereum_blockchain_height%7Binstance%3D~%22bootnode%3A9545%7Cminernode%3A9545%7Cnode1%3A9545%7Cnode2%3A9545%7Cnode3%3A9545%7Crpcnode%3A9545%22%7D)"
+
+	os.chdir(directory_location)
+
+	run_script(url, total_nodes)
